22ff01c2b592335902c876a4c6af3dc285b9b5f5
EefjeHelwegen <50697540+1810278helwegen@users.noreply.github.com>
2019-05-16 11:54:07 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//console.log("willy ging naar de tandarts");
+
+
+//1 selector
+//2 functie
+//3 parameter
+$("#button").click( function(){
+    $("#button").css("color" , "#fff");
+//    $("body").fadeOut(10000);
+    $("#button").animate({"margin-right" : "400px"}, 5000);
+});
+
+//3000 is 3 seconden
